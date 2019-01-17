51eaaf53856d8387c3e619bcd749e17b737ccf56
Steve Snyder <steve@rmer.info>
2019-01-17 17:09:18 +0000

    fix ssl handling

--- middleware/handle.js
+++ middleware/handle.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 module.exports = async ({ app, conf }) => {
 	const connInfo = conf.get('pgsql');
 	if (connInfo.ssl) {
-		Object.values(connInfo.ssl).forEach((key, val) => {
+		Object.entries(connInfo.ssl).forEach(([ key, val ]) => {
 			if (typeof val === 'string' && (/^[~.\/]/).test(val) && fs.existsSync(val)) {
-				connInfo.ssl[key] = fs.readFileSync(val);
+				connInfo.ssl[key] = fs.readFileSync(val).toString();
 			}
 		});
 	}
